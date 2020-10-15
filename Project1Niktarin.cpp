ffcf7a67487563bf80e3176fe44c2d7d77754764
Nikita <72571988+NektarinDevOps@users.noreply.github.com>
2020-10-15 18:06:56 +0300

    Добавьте файлы проекта.

---
+++ Project1Niktarin.cpp
@@ -0,0 +1,26 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int a, b, c, d = 3;
+  
+    cout << "Введите первое число:" ;
+    cin >> a;
+
+    cout << "Введите второе число:" ;
+    cin >> b;
+
+    cout << "Введите третье число:" ;
+    cin >> c;
+
+    cout << "Сумма этих чисел равно:" << a + b + c<<endl;
+    cout << "Произведение этих чисел равно:" << a * b * c<<endl;
+    cout << "Среднее арифметическое этих чисел равно:" << (a + b + c) / d << endl;
+
+
+
+}
